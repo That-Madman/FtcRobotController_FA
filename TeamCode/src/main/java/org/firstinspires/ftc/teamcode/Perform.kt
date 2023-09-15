7351bbc38affb91e907d720bd6c722922fb183c2
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-09-15 19:43:20 -0400

    Some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perform.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perform.kt
@@ -1,21 +1,20 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 
 @TeleOp
-public class Perform extends OpMode {
-    Stage_Directions dir = new Stage_Directions();
-    @Override
-    public void init() {
-        dir.drumRoll(hardwareMap);
+class Perform : OpMode() {
+    var dir = Stage_Directions()
+    override fun init() {
+        dir.drumRoll(hardwareMap)
     }
 
-    @Override
-    public void loop() {
+    override fun loop() {
         dir.driveFieldRelative(
-                -gamepad1.left_stick_y,
-                 gamepad1.left_stick_x,
-                 gamepad1.right_stick_x);
+            -gamepad1.left_stick_y.toDouble(),
+            gamepad1.left_stick_x.toDouble(),
+            gamepad1.right_stick_x.toDouble()
+        )
     }
 }
