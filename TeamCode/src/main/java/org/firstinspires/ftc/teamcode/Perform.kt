e5f69ab8da57bf247310248ae4ad4920a815bdbd
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-09-15 20:27:38 -0400

    Some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perform.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Perform.kt
@@ -6,15 +6,30 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 @TeleOp
 class Perform : OpMode() {
     var dir = Stage_Directions()
+
+    var northEnabled = true
+    var held = false
+
     override fun init() {
         dir.drumRoll(hardwareMap)
     }
 
     override fun loop() {
-        dir.driveFieldRelative(
-            -gamepad1.left_stick_y.toDouble(),
-            gamepad1.left_stick_x.toDouble(),
-            gamepad1.right_stick_x.toDouble()
-        )
+        if (gamepad1.left_stick_button && !held) northEnabled = !northEnabled
+
+        if (northEnabled) {
+            dir.driveFieldRelative(
+                gamepad1.left_stick_y.toDouble(),
+                -gamepad1.left_stick_x.toDouble(),
+                gamepad1.right_stick_x.toDouble()
+            )
+        } else {
+            dir.drive(
+                gamepad1.left_stick_y.toDouble(),
+                -gamepad1.left_stick_x.toDouble(),
+                gamepad1.right_stick_x.toDouble()
+            )
+        }
+        held = gamepad1.left_stick_button
     }
 }
