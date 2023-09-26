216e64e48f4cd1426907e8de9a69d92c57ed2404
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-09-26 15:57:56 -0400

    Some changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTele.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class FirstTele extends OpMode {
+    boolean trueNorth = false;
+    boolean buttonHeld = false;
+    Stage_Directions board = new Stage_Directions();
+
+    @Override
+    public void init() {
+        board.getHW(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        if (gamepad1.y && !buttonHeld)
+            trueNorth = !trueNorth;
+
+        board.driveFieldRelative(
+                -gamepad1.left_stick_y,
+                gamepad1.left_stick_x,
+                gamepad1.right_stick_x
+        );
+
+        buttonHeld = gamepad1.y;
+    }
+}
