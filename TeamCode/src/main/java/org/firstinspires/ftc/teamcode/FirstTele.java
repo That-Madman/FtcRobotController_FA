e35f2f27584d4dce7e2de8be641fab72376c4ef1
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-09-26 17:11:13 -0400

    Some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTele.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -17,21 +16,12 @@ public class FirstTele extends OpMode {
 
     @Override
     public void loop() {
-        if (gamepad1.y && !yHeld)
-            trueNorth = !trueNorth;
+        if (gamepad1.y && !yHeld) trueNorth = !trueNorth;
 
         if (trueNorth) {
-            board.driveFieldRelative(
-                    -gamepad1.right_stick_y,
-                    gamepad1.right_stick_x,
-                    gamepad1.left_stick_x
-            );
+            board.driveFieldRelative(-gamepad1.right_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
         } else {
-            board.drive(
-                    -gamepad1.right_stick_y,
-                    gamepad1.right_stick_x,
-                    gamepad1.left_stick_x
-            );
+            board.drive(-gamepad1.right_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
         }
         yHeld = gamepad1.y;
     }
