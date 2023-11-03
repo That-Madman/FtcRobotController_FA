e4a3b3593681e46bde5f65fb9cbcdf97b774a44a
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-03 18:00:57 -0400

    Merge remote-tracking branch 'origin/Robot_Code' into Robot_Code

--- TeamCode/src/main/java/autoThings/PID.kt
+++ TeamCode/src/main/java/autoThings/PID.kt
@@ -7,7 +7,7 @@ package autoThings
  * @param kD the D value
  * @param posGet the function to get the current value of what is being influenced
  * @param exFun the function executed when pidCalc is called
- * @param timeGet the function to get the time passed
+ * @param timeGet the function to get the time
  * @property pidCalc calculates the PID value and executes exFun if given
  * @property resetI resets the I value that gets accrued as the program runs
  * @author Alex Bryan
@@ -21,9 +21,23 @@ class PID @JvmOverloads constructor(
     private var exFun: ((Number) -> Unit)? = null,
     private var timeGet: (() -> Number)? = { System.nanoTime() / 1e9 },
 ) {
+    /**
+     * In PID, the I value is a value that gets aggregated while the formula goes on. It increases
+     * based off the difference between the current and target position, multiplied by [kI]. It is
+     * used to address a constant error, slowly increasing or decreasing to help sustain the target
+     * position. If [kP] is 0, i will never change, which is sometimes the best option.
+     */
     private var i: Double = 0.0
     private var maxI: Double = Double.NaN
 
+    /**
+     * Calculates the output for PID based off of the P, I, and D values.
+     * If [exFun] was set, the function also executes it.
+     * @param target The target value
+     * @param currPos The current value. If it is not set, the function will instead use [posGet]
+     * @param time The current time. If it is not set, the function will instead use [timeGet]
+     * @author Alex Bryan
+     */
     @JvmOverloads
     fun pidCalc(
         target: Number, currPos: Number = getPos(), time: Number = getTime()
@@ -44,6 +58,10 @@ class PID @JvmOverloads constructor(
         return p + i + d
     }
 
+    /**
+     * Resets the [i] value back to 0
+     * @author Alex Bryan
+     */
     fun resetI() {
         i = 0.0
     }
