c16efba8cbebcb5462ebfcfa4f627797042f8653
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-09 16:19:02 -0500

    Merge remote-tracking branch 'upstream/master'

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAuto.java
@@ -11,44 +11,45 @@ import java.util.List;
 @Autonomous
 public class FirstAuto extends LinearOpMode {
     Board board = new Board();
-    int spikeSpot = 0;
+    volatile int spikeSpot = 0;
+
 
     public void runOpMode() {
-        try{
-        board.getHW(hardwareMap, telemetry);} catch (Exception ignored){}
-        new Thread() {
-            public void run() {
-                try {
-                    List<Recognition> view = Collections.emptyList();
-                    while (view.size() == 0 && !opModeIsActive()) {
-                        view = board.getEyes().getTfod().getRecognitions();
-                    }
-                    if (board.getEyes().getTfod().getRecognitions().size() != 0) {
-                        if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 <= 240) {
-                            spikeSpot = 1;
-                        } else if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 > 240
-                                && (view.get(0).getLeft() + view.get(0).getRight()) / 2 < 480) {
-                            spikeSpot = 2;
-                        } else if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 >= 480) {
-                            spikeSpot = 3;
-                        }
-                    }
-                    telemetry.addData("spike found at:", (spikeSpot != 0) ? spikeSpot : "n/a");
-                } catch (Exception e) {
-                    telemetry.addData("Error in using camera because:", e);
-                } finally {
-                    try {
-                        board.getEyes().getVisionPortal().close();
-                    } catch (Exception e) {
-                        telemetry.addData("Problem ending vision portal because: ", e);
-                    }
+        try {
+            board.getHW(hardwareMap, telemetry);
+        } catch (Exception ignored) {
+        }
+
+        try { //start of TensorFlow
+            List<Recognition> view = Collections.emptyList();
+            while (view.size() == 0 && !opModeIsActive()) {
+                view = board.getEyes().getTfod().getRecognitions();
+            }
+            if (board.getEyes().getTfod().getRecognitions().size() != 0) {
+                if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 <= 240) {
+                    spikeSpot = 1;
+                } else if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 > 240 && (view.get(0).getLeft() + view.get(0).getRight()) / 2 < 480) {
+                    spikeSpot = 2;
+                } else if ((view.get(0).getLeft() + view.get(0).getRight()) / 2 >= 480) {
+                    spikeSpot = 3;
                 }
             }
-        }.start();
+            telemetry.addData("spike found at:", (spikeSpot != 0) ? spikeSpot : "n/a");
+        } catch (Exception e) {
+            telemetry.addData("Error in using camera because:", e);
+        } finally {
+            try {
+                board.getEyes().getVisionPortal().close();
+            } catch (Exception e) {
+                telemetry.addData("Problem ending vision portal because: ", e);
+            }
+        } //end of tensorFlow
+
+
         waitForStart();
+
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-
             }
         }
     }
