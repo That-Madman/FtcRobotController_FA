0ad9d4b833eabff6bd5f882ca1038ac3474c71eb
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-20 11:44:51 -0500

    \|/ |-| /-\ -|-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/USE_THIS_AUTO_IF_WE_ARE_DOOMED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/USE_THIS_AUTO_IF_WE_ARE_DOOMED.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
-public class USE_THIS_AUTO extends LinearOpMode {
+public class USE_THIS_AUTO_IF_WE_ARE_DOOMED extends LinearOpMode {
     Board board = new Board();
     @Override
     public void runOpMode() throws InterruptedException {
