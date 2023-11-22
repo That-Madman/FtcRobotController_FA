fec78daaed224dbf6afb30fd6064a900202028fb
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-22 11:04:41 -0500

    \|/ |-| /-\ -|-

--- TeamCode/src/main/java/autoThings/roadRunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/autoThings/roadRunner/drive/SampleMecanumDrive.java
@@ -61,7 +61,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
-    public static double LATERAL_MULTIPLIER = 1;
+    public static double LATERAL_MULTIPLIER = 0.906562741;
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
     public static double OMEGA_WEIGHT = 1;
