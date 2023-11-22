6a2f476d35c947437ae27373e77154370d739246
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-22 15:03:08 -0500

    \|/ |-| /-\ -|-

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
@@ -1,4 +1,33 @@
 package com.example.meepmeep
 
-class MeepMeep {
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.noahbres.meepmeep.MeepMeep
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
+import com.noahbres.meepmeep.roadrunner.DriveShim
+
+object MeepMeep {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val meepMeep = MeepMeep(800)
+        val myBot =
+            DefaultBotBuilder(meepMeep) // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60.0, 60.0, Math.toRadians(180.0), Math.toRadians(180.0), 15.0)
+                .followTrajectorySequence { drive: DriveShim ->
+                    drive.trajectorySequenceBuilder(Pose2d(0.0, 0.0, 0.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .forward(30.0)
+                        .turn(Math.toRadians(90.0))
+                        .build()
+                }
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+            .setDarkMode(true)
+            .setBackgroundAlpha(0.95f)
+            .addEntity(myBot)
+            .start()
+    }
 }
