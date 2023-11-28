c18141e13073ea9ecc33ff0266911fa8987f81ab
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-11-28 17:46:32 -0500

    \|/ |-| /-\ -|-

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeep.kt
@@ -1,7 +1,9 @@
 package com.example.meepmeep
 
 import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.noahbres.meepmeep.MeepMeep
+import com.noahbres.meepmeep.core.toRadians
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder
 import com.noahbres.meepmeep.roadrunner.DriveShim
 
@@ -19,15 +21,20 @@ object MeepMeep {
                     15.0)
                 .followTrajectorySequence { drive: DriveShim ->
                     drive.trajectorySequenceBuilder(Pose2d(
-                        0.0, 0.0, 0.0))
-                        .forward(30.0)
-                        .turn(Math.toRadians(90.0))
-                        .forward(30.0)
-                        .turn(Math.toRadians(90.0))
-                        .forward(30.0)
-                        .turn(Math.toRadians(90.0))
-                        .forward(30.0)
-                        .turn(Math.toRadians(90.0))
+                        -36.0, 61.0, 270.0.toRadians()))
+                        .lineToConstantHeading(Vector2d(-36.0, 35.0))
+                        .addDisplacementMarker{}
+                        .lineToConstantHeading(Vector2d(-36.0, 40.0))
+                        .splineToConstantHeading(Vector2d(-56.0, 50.0), 135.0.toRadians())
+                        .lineToConstantHeading(Vector2d(-56.0, 12.0))
+                        .lineToConstantHeading(Vector2d(-55.0, 12.0))
+                        .splineTo(Vector2d(-20.0, 0.0), 270.0.toRadians())
+                        .lineToConstantHeading(Vector2d(20.0, 0.0))
+                        .addDisplacementMarker{}
+                        .splineToSplineHeading(Pose2d(50.0, 30.0, 0.0), 0.0)
+                        .addDisplacementMarker{}
+                        .lineToConstantHeading(Vector2d(45.0, 30.0))
+                        .splineToConstantHeading(Vector2d(62.0, 12.0), 10.0.toRadians())
                         .build()
                 }
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
