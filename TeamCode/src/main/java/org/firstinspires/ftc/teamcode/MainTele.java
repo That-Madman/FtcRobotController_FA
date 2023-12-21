1e84654dca592052e4b6d5c3540953182606be20
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2023-12-21 15:57:58 -0500

    \|/ |-| /-\ -|-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTele.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOp")
-public class FirstTele extends OpMode {
+public class MainTele extends OpMode {
     Board board = new Board();
 
     boolean trueNorth,
