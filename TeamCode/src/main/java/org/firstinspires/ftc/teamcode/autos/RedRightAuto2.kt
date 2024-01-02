1a9dc4e061a8dc2fd3d4739351a097fa995e1343
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-02 11:18:57 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedRightAuto2.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedRightAuto2.kt
@@ -52,7 +52,7 @@ class RedRightAuto2 : OpMode() {
             .build()
 
         spike2 = drive!!.trajectorySequenceBuilder(drive!!.poseEstimate)
-            .splineToConstantHeading(Vector2d(12.0, -39.0), toRadians(90.0))
+            .splineToConstantHeading(Vector2d(12.0, -38.0), toRadians(90.0))
             .splineToLinearHeading(Pose2d(12.0, -36.0, toRadians(270.0)), toRadians(90.0))
             .splineToConstantHeading(Vector2d(12.0, -34.0), toRadians(90.0))
             .build()
