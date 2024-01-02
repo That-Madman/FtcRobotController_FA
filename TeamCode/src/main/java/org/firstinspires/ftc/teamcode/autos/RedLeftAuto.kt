be65cddcedfacc9b9ab59931de679c6320824419
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-02 14:36:00 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedLeftAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedLeftAuto.kt
@@ -6,7 +6,6 @@ import autoThings.slideHeight
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.firstinspires.ftc.teamcode.Board
 import java.lang.Math.toRadians
