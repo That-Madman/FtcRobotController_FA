3e8f4ec578d256fcb9c55c8efa808f7b1fe18774
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-02 13:19:01 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DoomedBlueLeftAuto.kt
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.autos
+
+import autoThings.roadRunner.drive.SampleMecanumDrive
+import autoThings.roadRunner.trajectorysequence.TrajectorySequence
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import java.lang.Math.toRadians
+
+@Autonomous(name = "WE ARE DOOMED (BLUE LEFT)")
+class DoomedBlueLeftAuto : OpMode() {
+
+    private var drive: SampleMecanumDrive? = null
+    private var park: TrajectorySequence? = null
+
+    override fun init() {
+        drive = SampleMecanumDrive(hardwareMap)
+
+        drive!!.poseEstimate = Pose2d(12.0, 61.0, toRadians(270.0))
+        park = drive!!.trajectorySequenceBuilder(drive!!.poseEstimate)
+            .lineToConstantHeading(Vector2d(12.0, 59.0))
+            .lineTo(Vector2d(59.0, 59.0))
+            .build()
+
+        drive!!.followTrajectorySequenceAsync(park)
+    }
+
+    override fun loop() {
+        drive!!.update()
+    }
+}
