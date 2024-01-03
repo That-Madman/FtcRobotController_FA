079b973addcdde68a877b7ef56b6b82a96d7c1fd
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-03 11:28:51 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOps/TeleK.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOps/TeleK.kt
@@ -17,6 +17,14 @@ class TeleK : OpMode() {
 
     override fun init() {
         board.getHW(hardwareMap, telemetry)
+
+        try {
+            board.setIntakeLift(0.0)
+        } catch (_: Throwable) {
+            telemetry.addLine("The servo intake lifter has an issue.")
+        }
+
+        telemetry.update()
     }
 
     override fun loop() {
