3a2d7b329540b07754705696a8a80737429b3aaf
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-22 18:20:38 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skitter.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skitter.kt
@@ -7,11 +7,18 @@ import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorImplEx
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 import com.qualcomm.robotcore.hardware.IMU
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.hypot
+import kotlin.math.max
+import kotlin.math.sin
 
 @TeleOp
 class Skitter : OpMode() {
     private val driveBase = arrayOfNulls<DcMotorImplEx>(4)
-    private var imu : IMU? = null
+    private var imu: IMU? = null
 
     override fun init() {
         driveBase[0] = hardwareMap.get(DcMotorImplEx::class.java, "frontLeft")
@@ -42,5 +49,47 @@ class Skitter : OpMode() {
 
     override fun loop() {
 
+    }
+
+    private fun driveFieldRelative(forward: Double, right: Double, rotate: Double) {
+        val robotAngle = imu!!.robotYawPitchRollAngles.getYaw(AngleUnit.RADIANS)
+        var theta = atan2(forward, right)
+        val r = hypot(forward, right)
+        theta = AngleUnit.normalizeRadians(theta - robotAngle)
+        val newForward = r * sin(theta)
+        val newRight = r * cos(theta)
+
+        drive(newForward, newRight, rotate)
+    }
+
+    private fun drive(forward: Double, right: Double, rotate: Double) {
+        val frontLeftPower = forward + right + rotate
+        val frontRightPower = forward - right - rotate
+        val backLeftPower = forward - right + rotate
+        val backRightPower = forward + right - rotate
+
+        setPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower)
+    }
+
+    private fun setPowers(
+        frontLeftPower: Double,
+        frontRightPower: Double,
+        backLeftPower: Double,
+        backRightPower: Double
+    ) {
+        val powers = arrayOf(frontLeftPower, frontRightPower, backLeftPower, backRightPower)
+        var maxSpeed = 1.0
+
+        for (i in powers) {
+            maxSpeed = max(maxSpeed, abs(i))
+        }
+
+        for (i in powers.indices) {
+            powers[i] /= maxSpeed
+        }
+
+        for (i in powers.indices) {
+            driveBase[i]?.power = powers[i]
+        }
     }
 }
