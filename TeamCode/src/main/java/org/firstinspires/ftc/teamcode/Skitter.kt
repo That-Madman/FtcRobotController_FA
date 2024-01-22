7782be0dd21ac30b76973e89b7365a05e9eecca3
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-01-22 18:09:13 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Skitter.kt
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+
+@TeleOp
+class Skitter : OpMode() {
+    override fun init() {
+
+    }
+
+    override fun loop() {
+
+    }
+}
