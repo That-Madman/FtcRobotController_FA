a0f3d7cc71a0753f26a2643e9f0c36aee235e847
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-03-05 17:26:20 -0500

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dance.kt
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+
+class Dance : OpMode(){
+    override fun init() {
+
+    }
+
+    override fun loop() {
+
+    }
+}
