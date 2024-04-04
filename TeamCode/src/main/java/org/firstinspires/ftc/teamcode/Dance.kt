e16d716dbbd1154d3cf2aa3fa91f902f281c0915
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-04-04 17:38:26 -0400

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dance.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dance.kt
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import props
 
-class Dance : OpMode(){
+class Dance : OpMode() {
+
+    private var props : props? = null;
+
     override fun init() {
-
+        props = props(hardwareMap)
     }
 
     override fun loop() {
---
+++ TeamCode/src/main/java/props.kt
@@ -0,0 +1,20 @@
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.HardwareMap
+import com.qualcomm.robotcore.hardware.IMU
+
+class props (hwMap: HardwareMap) {
+
+    val driveBase: Array<DcMotorEx?> = arrayOfNulls(4)
+    var imu : IMU? = null
+
+    init {
+        for (i in driveBase.indices) {
+            driveBase[i] = hwMap.get(DcMotorEx::class.java, arrayOf("rf", "rb", "lf", "lb")[i])
+            driveBase[i]?.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        }
+
+        imu = hwMap.get(IMU::class.java, "imu")
+    }
+
+}
