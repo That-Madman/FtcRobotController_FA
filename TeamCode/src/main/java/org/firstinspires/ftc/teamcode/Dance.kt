e7bccb473b15472818800f7fc86dafac1d922ae0
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-04-09 16:42:28 -0400

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dance.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Dance.kt
@@ -5,13 +5,17 @@ import props
 
 class Dance : OpMode() {
 
-    private var props : props? = null;
+    private var props: props? = null;
 
     override fun init() {
         props = props(hardwareMap)
     }
 
     override fun loop() {
-
+        props!!.driveFieldRelative(
+            -gamepad1.left_stick_y.toDouble(),
+            gamepad1.left_stick_x.toDouble(),
+            gamepad1.right_stick_x.toDouble()
+        )
     }
 }
