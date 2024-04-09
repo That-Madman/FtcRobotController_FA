103aafdd4218989d5e5f266edb10dc4759495bfe
Ramss07 <92797156+Ramss07@users.noreply.github.com>
2024-04-09 16:30:38 -0400

    \|/ |-| /-\ ‾|‾ ' _|‾   ‾|‾ |-| | _|‾ ?
    -Radiohead

--- TeamCode/src/main/java/props.kt
+++ TeamCode/src/main/java/props.kt
@@ -3,11 +3,18 @@ import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.HardwareMap
 import com.qualcomm.robotcore.hardware.IMU
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
+import kotlin.math.abs
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.hypot
+import kotlin.math.max
+import kotlin.math.sin
 
-class props (hwMap: HardwareMap) {
+class props(hwMap: HardwareMap) {
 
     private val driveBase: Array<DcMotorEx?> = arrayOfNulls(4)
-    private var imu : IMU? = null
+    private var imu: IMU? = null
 
     init {
         for (i in driveBase.indices) {
@@ -22,7 +29,49 @@ class props (hwMap: HardwareMap) {
                     RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
                     RevHubOrientationOnRobot.UsbFacingDirection.UP
                 )
-        ))
+            )
+        )
+    }
 
+    private fun setPowers(
+        frontLeftPower: Double,
+        frontRightPower: Double,
+        backLeftPower: Double,
+        backRightPower: Double
+    ) {
+        val powers = arrayOf(frontLeftPower, frontRightPower, backLeftPower, backRightPower)
+        var maxSpeed = 1.0
+
+        for (i in powers) {
+            maxSpeed = max(maxSpeed, abs(i))
+        }
+
+        for (i in powers.indices) {
+            powers[i] /= maxSpeed
+        }
+
+        for (i in powers.indices) {
+            driveBase[i]?.power = powers[i]
+        }
+    }
+
+    fun drive(forward: Double, right: Double, rotate: Double) {
+        val frontLeftPower = forward + right + rotate
+        val frontRightPower = forward - right - rotate
+        val backLeftPower = forward - right + rotate
+        val backRightPower = forward + right - rotate
+
+        setPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower)
+    }
+
+    fun driveFieldRelative(forward: Double, right: Double, rotate: Double) {
+        val robotAngle = imu!!.robotYawPitchRollAngles.getYaw(AngleUnit.RADIANS)
+        var theta = atan2(forward, right)
+        val r = hypot(forward, right)
+        theta = AngleUnit.normalizeRadians(theta - robotAngle)
+        val newForward = r * sin(theta)
+        val newRight = r * cos(theta)
+
+        drive(newForward, newRight, rotate)
     }
 }
